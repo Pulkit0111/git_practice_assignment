cc30ed8f269b9c221b77e8ac93090f41d9ce08f1
Pulkit Tyagi <pulkittyagi@192.168.0.196>
2022-09-19 21:16:01 +0530

    added palindrome and prime problem

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+let string="naman"
+
+
+function reverse(str){
+	let bag="";
+	for (let i=str.length-1;i>=0;i--){
+		bag=bag+str[i];
+	}
+	return bag;
+}
+
+let reverse_str=reverse(string);
+
+if(string===reverse_str){
+	console.log("palindrome");
+} else {
+	console.log("not palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,11 @@
+
+function prime(num){
+	for (let i=1;i<=num;i++){
+		if(num%i==0){
+			return true;
+		}
+		return false;
+	}
+}
+
+prime(13);
